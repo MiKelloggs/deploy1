84f88fef1978abc4611189e93593a6b9534dfbd2
MiKelloggs <korologos.michael0@gmail.com>
2017-04-27 10:00:31 -0600

    f

--- server.py
+++ server.py
@@ -1,46 +1,146 @@
-import os
-import psycopg2
-import psycopg2.extras
-import urllib.parse
+import json
+import sys
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+from squirrel_db import SquirrelDB
 
-class GlassDB:
+class SquirrelServerHandler(BaseHTTPRequestHandler):
 
-    def __init__(self):
-        urllib.parse.uses_netloc.append("postgres")
-        url = urllib.parse.urlparse(os.environ["http://mkorologos.com/3200/Deploy/glass.db"])
+    # METHODS
 
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin",  "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Accept, Content-Type, Origin")
+        self.end_headers()
 
-        self.connection = psycopg2.connect("http://mkorologos.com/3200/Deploy/glass.db")
-        self.connection.row_factory = dict_factory
-        self.cursor = self.connection.cursor()
-        return
+    def do_GET(self):
+        resourceName, resourceId = self.parsePath()
+        if resourceName == "squirrels":
+            if resourceId:
+                self.handleSquirrelsRetrieve(resourceId)
+            else:
+                self.handleSquirrelsIndex()
+        else:
+            self.handle404()
 
-    def __del__(self):
-        self.connection.close()
-        return
+    def do_POST(self):
+        resourceName, resourceId = self.parsePath()
+        if resourceName == "squirrels":
+            if resourceId:
+                self.handle404()
+            else:
+                self.handleSquirrelsCreate()
+        else:
+            self.handle404()
 
-    def getGlass(self):
-        self.cursor.execute("SELECT * FROM glassdb")
-        return self.cursor.fetchall()
+    def do_PUT(self):
+        resourceName, resourceId = self.parsePath()
+        if resourceName == "squirrels":
+            if resourceId:
+                self.handleSquirrelsUpdate(resourceId)
+            else:
+                self.handle404()
+        else:
+            self.handle404()
 
-    def createGlass(self, messages):
-        self.cursor.execute("INSERT INTO glassdb (yearstart, yearend, make, model, partnumber, location, cost, stock) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (messages[0], messages[1], messages[2], messages[3], messages[4], messages[5], messages[6], messages[7]))
-        self.connection.commit()
-        return
+    def do_DELETE(self):
+        resourceName, resourceId = self.parsePath()
+        if resourceName == "squirrels":
+            if resourceId:
+                self.handleSquirrelsDelete(resourceId)
+            else:
+                self.handle404()
+        else:
+            self.handle404()
 
-    def modifyGlass(self, messages):
-        self.cursor.execute("DELETE FROM glassdb WHERE partnumber = ?",  (messages[4],))
-        self.cursor.execute("INSERT INTO glassdb (yearstart, yearend, make, model, partnumber, location, cost, stock) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (messages[0], messages[1], messages[2], messages[3], messages[4], messages[5], messages[6], messages[7]))
-        self.connection.commit()
-        return
+    # HELPERS
 
-    def deleteGlass(self, data):
-        self.cursor.execute("DELETE FROM glassdb WHERE partnumber = ?",  (data,))
-        self.connection.commit()
-        return
+    def getRequestData(self):
+        length = int(self.headers["Content-Length"])
+        body = self.rfile.read(length).decode("utf-8")
+        data = parse_qs(body)
+        for key in data:
+            data[key] = data[key][0]
+        return data
 
-    def createGlassTable(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS glassdb (id SERIAL PRIMARY KEY, yearstart INTEGER, yearend INTEGER, make VARCHAR(255), model VARCHAR(255), partnumber VARCHAR(255), location VARCHAR(255), cost INTEGER, stock INTEGER)")
-        self.connection.commit()
+    def parsePath(self):
+        if self.path.startswith("/"):
+            parts = self.path[1:].split("/")
+            resourceName = parts[0]
+            resourceId = None
+            if len(parts) > 1:
+                resourceId = parts[1]
+            return (resourceName, resourceId)
+        return False
 
-        
+    # ACTIONS
+
+    def handleSquirrelsIndex(self):
+        db = SquirrelDB()
+        squirrelsList = db.getSquirrels()
+        self.send_response(200)
+        self.send_header("Content-Type",  "application/json")
+        self.end_headers()
+        self.wfile.write(bytes(json.dumps(squirrelsList), "utf-8"))
+
+    def handleSquirrelsRetrieve(self, squirrelId):
+        db = SquirrelDB()
+        squirrel = db.getSquirrel(squirrelId)
+        if squirrel:
+            self.send_response(200)
+            self.send_header("Content-Type",  "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(json.dumps(squirrel), "utf-8"))
+        else:
+            self.handle404()
+
+    def handleSquirrelsCreate(self):
+        db = SquirrelDB()
+        db.createSquirrel(self.getRequestData())
+        self.send_response(201)
+        self.end_headers()
+
+    def handleSquirrelsUpdate(self, squirrelId):
+        db = SquirrelDB()
+        squirrel = db.getSquirrel(squirrelId)
+        if squirrel:
+            db.updateSquirrel(squirrelId, self.getRequestData())
+            self.send_response(204)
+            self.end_headers()
+        else:
+            self.handle404()
+
+    def handleSquirrelsDelete(self, squirrelId):
+        db = SquirrelDB()
+        squirrel = db.getSquirrel(squirrelId)
+        if squirrel:
+            db.deleteSquirrel(squirrelId)
+            self.send_response(204)
+            self.end_headers()
+        else:
+            self.handle404()
+
+    def handle404(self):
+        self.send_response(404)
+        self.send_header("Content-Type",  "text/plain")
+        self.end_headers()
+        self.wfile.write(bytes("404 Not Found", "utf-8"))
+
+def run():
+    db = SquirrelDB()
+    db.createSquirrelsTable()
+    db = None # disconnect
+
+    port = 8080
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+
+    listen = ("0.0.0.0", port)
+    server = HTTPServer(listen, SquirrelServerHandler)
+
+    print("Server listening on", "{}:{}".format(*listen))
+    server.serve_forever()
+
+run():
