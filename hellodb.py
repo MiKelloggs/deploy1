e156b72a4d6d6bc2f8f3ea424300ac4d7602f373
MiKelloggs <korologos.michael0@gmail.com>
2017-04-27 11:19:36 -0600

    d

--- hellodb.py
+++ hellodb.py
@@ -2,10 +2,7 @@ import sys
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 from urllib.parse import urlparse, parse_qs
-import os
-import psycopg2
-import psycopg2.extras
-import urllib.parse
+from server import GlassDB
 
 fw = open('database.txt', 'w')
 fw.write("testing 1i23")
@@ -132,64 +129,6 @@ class httpServerRequsetHandler(BaseHTTPRequestHandler):
             self.send_header("Content-type", "text/html")#name of header and value
             self.end_headers()
             self.wfile.write(bytes("<strong>Not Found</strong>", "utf-8")) #dont write just a string, http doesnt understand ordinary strings.
-
-
-
-class GlassDB:
-
-    def __init__(self):
-        urllib.parse.uses_netloc.append("postgres")
-        url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
-
-
-        self.connection.row_factory = dict_factory
-
-
-        self.connection = psycopg2.connect(
-            cursor_factory=psycopg2.extras.RealDictCursor,
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-        )
-
-        self.cursor = self.connection.cursor()
-
-    def __del__(self):
-        self.connection.close()
-        return
-
-    def getGlass(self):
-        self.cursor.execute("SELECT * FROM glassdb")
-        return self.cursor.fetchall()
-
-    def createGlass(self, messages):
-        self.cursor.execute("INSERT INTO glassdb (yearstart, yearend, make, model, partnumber, location, cost, stock) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (messages[0], messages[1], messages[2], messages[3], messages[4], messages[5], messages[6], messages[7]))
-        self.connection.commit()
-        return
-
-    def modifyGlass(self, messages):
-        self.cursor.execute("DELETE FROM glassdb WHERE partnumber = ?",  (messages[4],))
-        self.cursor.execute("INSERT INTO glassdb (yearstart, yearend, make, model, partnumber, location, cost, stock) VALUES(?, ?, ?, ?, ?, ?, ?, ?)", (messages[0], messages[1], messages[2], messages[3], messages[4], messages[5], messages[6], messages[7]))
-        self.connection.commit()
-        return
-
-    def deleteGlass(self, data):
-        self.cursor.execute("DELETE FROM glassdb WHERE partnumber = ?",  (data,))
-        self.connection.commit()
-        return
-
-    def createGlassTable(self):
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS glassdb (id SERIAL PRIMARY KEY, yearstart INTEGER, yearend INTEGER, make VARCHAR(255), model VARCHAR(255), partnumber VARCHAR(255), location VARCHAR(255), cost INTEGER, stock INTEGER)")
-        self.connection.commit()
-
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
 
 
 def main():
